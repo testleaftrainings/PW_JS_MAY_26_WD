301d4b07c482b72db4a675bcee007e28e251cefe
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-05-12 21:39:16 +0530

    --day01 uploads--

---
+++ week1/day1/learnDatatypes.js
@@ -0,0 +1,25 @@
+//  number datatype
+// 9 , 9.76, 8968368936836836
+// keyword identifierName = value
+var data1 = 9.142
+console.log(data1)
+
+// string datatype
+// "" --> double quotes,''--> single quotes,``--> back tick
+// keyword identifierName = value
+var data2=`welcome to playwright`
+console.log(typeof data2)
+
+// boolean datatype
+// true, false
+var data3=true
+console.log(data3)
+
+// undefined datatype
+var data4
+console.log(data4)
+
+// null datatype
+var data5=null
+console.log(typeof data5)
+
